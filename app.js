053ac08e5fede370ab6ad6679d48db4fa446be15
rodrigorozendobastos@gmail.com <rodrigorozendobastos@gmail.com>
2016-11-12 15:56:40 -0300

    Add view engine

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine", "jade");
 app.set("port", parseInt(process.env.PORT) || 3000);
-//.set('port', config.port) int(process.env.PORT)
 
 app.get("/", function (req, res) {
   res.send("Clube Mamãe em Maceió!");
