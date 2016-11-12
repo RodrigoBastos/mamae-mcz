4993d8cc058f3a35f5527a809ea76a714732f231
rodrigorozendobastos@gmail.com <rodrigorozendobastos@gmail.com>
2016-11-12 11:19:08 -0300

    Change get port

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require("express");
 var app = express();
 
+app.set("port", parseInt(process.env.PORT) || 3000);
+//.set('port', config.port) int(process.env.PORT)
 
 app.get("/", function (req, res) {
   res.send("Clube Mamãe em Maceió!");
 });
 
-app.listen(app.get('port'), function () {
- console.log("Servidor carregado na porta: ", app.get('port'));
+app.listen(app.get("port"), function () {
+ console.log("Servidor carregado na porta: ", app.get("port"));
 });
