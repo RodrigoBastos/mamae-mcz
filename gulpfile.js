2bcfc8d86149cc4e1a07b31d53d4d8633d7ad26e
rodrigorozendobastos@gmail.com <rodrigorozendobastos@gmail.com>
2016-11-15 12:33:07 -0300

    Cria  automatização css via gulp

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp =  require("gulp");
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+var minifyCss = require("gulp-minify-css");
+var rename = require("gulp-rename");
+
+var paths = {
+  scss: ["./client/source/scss/*.scss"],
+  js: ["./client/source/js/*.js"]
+};
+
+gulp.task("default", ["watch"]);
+
+gulp.task ("css", function () {
+
+  return gulp.src(paths.scss)
+    .pipe(sass({
+      outputStyle: 'compressed',
+      errLogToConsole: true
+    }))
+    .pipe(autoprefixer({
+      browsers: ['last 3 versions'],
+      cascade: false
+    }))
+    .pipe(minifyCss({ keepSpecialComments: 0 }))
+    .pipe(rename({  dirname: '/css', extname: '.min.css' }))
+    .pipe(gulp.dest('./client/public/'));
+
+});
+
+gulp.task("watch", function () {
+  gulp.watch(paths.scss, ["css"]);
+  gulp.watch(paths.js, ["js"]);
+});
