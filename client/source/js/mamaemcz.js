1f46bead8055b26145e52618a2bbe24fe930aea7
Samuel das Chagas <samueldaschagas@gmail.com>
2016-11-21 22:21:33 -0300

    Iniciando secao associe-se

--- client/source/js/mamaemcz.js
+++ client/source/js/mamaemcz.js
@@ -1,1 +1,23 @@
+$(document).ready(function() {
+ 
+   $("#owl-demo").owlCarousel({
+ 
+      items: 4,
+      loop: true,
+      autoplay:true,
+      autoplayTimeout:4000,
+      dots: false
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,https://www.cursosiag.com.br/lms/common/site/imgs/banners/banner_academia.jpg
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
+ 
+});
 
+window.sr = ScrollReveal();
+sr.reveal('.foo', { duration: 1000 }, 50);
