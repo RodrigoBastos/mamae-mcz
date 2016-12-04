ff7745a2652c4515d224b3fafcbad1ad1a60a460
rodrigorozendobastos@gmail.com <rodrigorozendobastos@gmail.com>
2016-12-03 21:54:36 -0300

    Adiciona favicon

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var path = require("path");
 var express = require("express");
+var favicon = require("serve-favicon");
 
 //Rotas
 var mainRoutes = require("./server/routes/mainRoutes");
@@ -16,6 +17,7 @@ function App () {
     .set("views", path.join(__dirname, "client", "views"))
     .set("port", parseInt(process.env.PORT || 5000))
     .use(express.static(path.join(__dirname, "client", "public")))
+    .use(favicon(__dirname + "/client/public/images/logo.png"))
     .use(mainRoutes);
 }
 
