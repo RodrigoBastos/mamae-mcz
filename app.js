92fd02965c3dfed284bcc571e879276f9c9faaa6
rodrigorozendobastos@gmail.com <rodrigorozendobastos@gmail.com>
2017-01-12 20:43:50 -0300

    Configura aplicativo express de acordo com o eslint

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
-const path = require('path');
-const express = require('express');
-const favicon = require('serve-favicon');
-const bodyParser = require('body-parser');
+var path = require('path');
+var express = require('express');
+var favicon = require('serve-favicon');
+var bodyParser = require('body-parser');
 
-//Rotas
-const mainRoutes = require('./server/routes/mainRoutes');
-const mailRoutes = require('./server/routes/mailRoutes');
+// Rotas
+var mainRoutes = require('./server/routes/mainRoutes');
+var mailRoutes = require('./server/routes/mailRoutes');
 
 /**
  * Aplicativo
  * @returns {*}
  * @constructor
  */
-function App () {
-
+function App() {
   return express()
     .set('view engine', 'jade')
     .set('views', path.join(__dirname, 'client', 'views'))
-    .set('port', parseInt(process.env.PORT || 4500))
+    .set('port', parseInt(process.env.PORT || 4500, 10))
     .use(express.static(path.join(__dirname, 'client', 'public')))
-    .use(favicon(__dirname + '/client/public/images/logo.png'))
+    .use(favicon(path.join(__dirname, '/client/public/images/logo.png')))
     .use(bodyParser.json())
     .use(bodyParser.urlencoded({ extended: false }))
     .use(mainRoutes)
--- cluster_app.js
+++ cluster_app.js
@@ -2,9 +2,8 @@ var cluster = require('cluster');
 var numCPUs = require('os').cpus().length;
 
 if (cluster.isMaster) {
-
   // Fork Workers
-  for (var i = 0; i < numCPUs; i++) {
+  for (var i = 0; i < numCPUs; i += 1) {
     cluster.fork();
   }
 
@@ -13,7 +12,7 @@ if (cluster.isMaster) {
     cluster.fork();
   });
 } else {
-
+  /* eslint-disable global-require */
   // Aplicativo express
   var App = require('./app');
   var app = new App();
@@ -25,5 +24,4 @@ if (cluster.isMaster) {
     'Express cluster number ' + cluster.worker.id +
     ' listening on port ' + app.get('port') +
     ' in ' + app.get('env') + ' mode');
-
 }
